b0d437b08f3a3118b7fcf1c50961da9eaa984e57
Manuel Villalpando L.om <manuvilla01@hotmail.com>
2024-04-15 20:00:06 -0600

    feat: Components, routes and login module

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginModule } from './login/login.module';
+import { LoginComponent } from './login/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent,    
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -6,7 +6,29 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+/* Now we add the routes */
 export class AppComponent {
   title = 'Front';
-recentFiles: any;
+  recentFiles: any;
+  constructor(private router: Router) {
+    this.recentFiles = [
+      {
+        name: 'File 1',
+        date: '01/01/2021'
+      },
+      {
+        name: 'File 2',
+        date: '01/02/2021'
+      },
+      {
+        name: 'File 3',
+        date: '01/03/2021'
+      }
+    ];
+  }
+  goTo(route: string) {
+    this.router.navigate([route]);
+  }
 }
+
