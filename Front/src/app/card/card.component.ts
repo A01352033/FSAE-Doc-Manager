2032b7746c6e6ba8f6fa9ac84ac8de6a9165cee4
F. N. Jervis <nicojervis@gmail.com>
2024-04-17 22:43:13 -0600

    Merge branch 'dev' of https://github.com/A01352033/FSAE-Doc-Manager into feature/sprint1-sidebar-josue

--- Front/src/app/card/card.component.ts
+++ Front/src/app/card/card.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from '../api.service'; // Update the path to the actual service path.
+import { Document } from '../document/document';
+import { Folder } from '../folder/folder';
 
 @Component({
   selector: 'app-card',
@@ -11,22 +14,50 @@ cards: any = [
     {
       title: 'Card 1',
       description: 'This is card 1',
-      image: 'https://via.placeholder.com/150'
+      image: '../assets/folder_icon.png'
     },
     {
       title: 'Card 2',
       description: 'This is card 2',
-      image: 'https://via.placeholder.com/150'
+      image: '../assets/folder_icon.png'
     },
     {
       title: 'Card 3',
       description: 'This is card 3',
-      image: 'https://via.placeholder.com/150'
+      image: '../assets/folder_icon.png'
     }
   ];
 
-  constructor() { }
+  documents: Document[] = [];
+
+  folders: Folder[] = [];
+  
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    console.log("init");
+    this.apiService.getRecentDocuments(1).subscribe({
+      next: (response: any[]) => {if (response && response['items']) {
+        this.documents = response['items'].map((item: any) => ({
+          id: item.id,
+          nombre: item.nombre,
+          fechacreada: new Date(item.fechacreada)
+        })); 
+      }},
+      error: (err) => console.error("err", err),
+    });
+
+    this.apiService.getFolders(1,1).subscribe({
+      next: (response: any[]) => {if (response && response['items']) {
+        this.folders = response['items'].map((item: any) => ({
+          id: item.id,
+          nombre: item.nombre,
+          fechacreada: new Date(item.fecha_creada)
+        })); 
+      }},
+      error: (err) => console.error("err", err),
+    });
+    console.log("docs", this.folders);
+
   }
 }
