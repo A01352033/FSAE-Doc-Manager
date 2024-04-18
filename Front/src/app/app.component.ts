7f18eaa24b6f304e6437d995d084b5910eeb9485
Manuel Villalpando L.om <manuvilla01@hotmail.com>
2024-04-17 23:30:45 -0600

    fix: sidebar

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,34 +1,45 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-
-/* Now we add the routes */
 export class AppComponent {
   title = 'Front';
   recentFiles: any;
+  isLanding: boolean = false;
+  isLogin: boolean = false;
+  isRegister: boolean = false;
+  isNothing: boolean = false;
+
   constructor(private router: Router) {
     this.recentFiles = [
-      {
-        name: 'File 1',
-        date: '01/01/2021'
-      },
-      {
-        name: 'File 2',
-        date: '01/02/2021'
-      },
-      {
-        name: 'File 3',
-        date: '01/03/2021'
-      }
+      { name: 'File 1', date: '01/01/2021' },
+      { name: 'File 2', date: '01/02/2021' },
+      { name: 'File 3', date: '01/03/2021' }
     ];
+
+    // Update the state based on the initial URL
+    this.updateRouteState(this.router.url);
+
+    // Listen for route changes
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateRouteState(event.url);
+      }
+    });
   }
+
+  updateRouteState(url: string) {
+    this.isNothing = url === '';
+    this.isLanding = url === '/landing';
+    this.isLogin = url === '/login';
+    this.isRegister = url === '/register';
+  }
+
   goTo(route: string) {
     this.router.navigate([route]);
   }
 }
-
