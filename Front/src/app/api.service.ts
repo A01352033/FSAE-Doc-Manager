8db63d9f19b5504941691e03525cc5f24a6d8f38
David-Cavazos <88981757+David-Cavazos@users.noreply.github.com>
2024-04-18 13:58:54 -0600

    api_post_put

--- Front/src/app/api.service.ts
+++ Front/src/app/api.service.ts
@@ -29,6 +29,34 @@ export class ApiService {
     );
   }
 
+  createDocument(documentData: Document): Observable<Document> {
+    return this.http.post<Document>('https://gb9eb75e0859bf4-techtitansdb.adb.mx-monterrey-1.oraclecloudapps.com/ords/admin/documento/', documentData, { headers: this.headers });
+  }
+
+  updateDocument(docId: string, documentData: Document): Observable<Document> {
+    const url = `https://gb9eb75e0859bf4-techtitansdb.adb.mx-monterrey-1.oraclecloudapps.com/ords/admin/documento/${docId}`; // Append the document ID to the base URL
+
+    const doc_update = {
+      id_folder: documentData.id_folder, 
+      nombre: documentData.nombre,
+      tipo_documento: documentData.nombre,
+      fechacreada:documentData.nombre, 
+      version_id: documentData.nombre, 
+      descripcion: documentData.nombre,
+      id_blob: documentData.nombre,
+      fechamodificado: documentData.nombre,
+    }
+    return this.http.put<Document>(url, doc_update, { headers: this.headers });
+  }
+
+
+
+
+
+
+
+
+
 
 
 
